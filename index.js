4c0f4bdf0c51f66184f531ae2a1bd335fe3d92e4
Paul Philips <paulphilips@email.com>
2025-03-11 19:56:44 +0530

    maintaining Multiple Routes (Catching all invalid routes)

--- index.js
+++ index.js
@@ -10,28 +10,18 @@ app.get("/", (req, res) => {
   res.send("Hello, Express kaise Ho");
 });
 
-app.use("/user", router);
-app.use(express.json());
-
-app.post("/users", (req, res) => {
-  const { name, email } = req.body;
+app.get("/things/:name/:id([0-9]{5})", (req, res) => {
+  const { name, id } = req.params;
   res.json({
-    message: `User ${name} with email ${email} Created Successfully`,
+    id,
+    name,
   });
 });
 
-app.put("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  const { name, email } = req.body;
-  res.json({
-    message: `User ${userId} updated to ${name} , ${email}`,
-  });
-});
-
-app.delete("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  res.json({
-    message: `User with ID ${userId} deleted Successfullys`,
+//catch All invalid routes
+app.get("*", (req, res) => {
+  res.send({
+    message: "Sorry, this is  an invalid URL",
   });
 });
 
